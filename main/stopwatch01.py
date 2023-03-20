7707332e788ccb4e4062b3979067778f9a7034c9
정승현 <tmdgusya@naver.com>
2023-03-21 00:25:04 +0900

    Add Stopwatch widgets

--- main/stopwatch01.py
+++ main/stopwatch01.py
@@ -1,16 +1,32 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
+from textual.containers import Container
+from textual.widgets import Header, Footer, Button, Static
+
+class TimeDisplay(Static):
+    """A widget to display elapsed time."""
+
+class Stopwatch(Static):
+    """A stopwatch widget."""
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the stopwatch."""
+        yield Button("Start", id = "start", variant = "success")
+        yield Button("Stop", id = "stop", variant = "error")
+        yield Button("Reset", id = "reset")
+        yield TimeDisplay("00:00:00.000")
 
 # @see https://textual.textualize.io/tutorial/
 class StopwatchApp(App):
     """A Textual app to manage stopwatched"""
 
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    # a list of tuples of (key, action, description)
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode"), ("i", "install", "Install the app")]
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
+        yield Container(Stopwatch(), Stopwatch(), Stopwatch())
 
     def action_toggle_dark(self) -> None:
         """An Action to toggle dark mode."""
